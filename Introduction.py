ad26b311ff0c89beace9933a1d2bf3be900c4216
Alok T <75138251+indianbot@users.noreply.github.com>
2021-04-24 23:26:17 +0530

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,9 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = 'Alok'
+>>> age = 29
+>>> salary = 36500.63
+>>> ismarried = False
+>>> print(name, age, salary, ismarried)
+Alok 29 36500.63 False
+>>> 
